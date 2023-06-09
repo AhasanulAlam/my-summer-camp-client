ffc29a9ec0e758e3abf43a2328673a06505e0777
AhasanulAlam <ahasanulalam@gmail.com>
2023-06-09 11:45:09 +0800

    Added TanStack Query and wrap the Router Provider

--- src/components/SingleClass/SingleClass.jsx
+++ src/components/SingleClass/SingleClass.jsx
@@ -4,28 +4,35 @@ import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const SingleClass = ({ singleClass }) => {
-    const { className, classImage, instructorName, instructorEmail, availableSeats, price, duration } = singleClass;
-    const {user} = useContext(AuthContext);
+    const { _id, className, classImage, instructorName, instructorEmail, availableSeats, price, duration } = singleClass;
+    const { user } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
 
-    const handleAddToCart = (singleClass) =>{
+    const handleAddToCart = (singleClass) => {
         console.log(singleClass);
-        if(user){
-            fetch(`http://localhost:5000/carts`,{})
-            .then(res => res.json())
-            .then(data => {
-                if(data.insertedId){
-                    Swal.fire({
-                        position: 'center',
-                        icon: 'success',
-                        title: `${className} Class added in the Cart successfully!`,
-                        showConfirmButton: false,
-                        timer: 1500
-                    })
-                }
+        if (user && user.email) {
+            const cartItem = {classItemId: _id, className, classImage, instructorName, instructorEmail, availableSeats, price, duration, email: user.email} 
+            fetch(`http://localhost:5000/carts`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(cartItem)
             })
-        }else{
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: `${className} Class added in the Cart successfully!`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                })
+        } else {
             Swal.fire({
                 title: 'Please login to Enroll the Class!',
                 icon: 'warning',
@@ -54,7 +61,7 @@ const SingleClass = ({ singleClass }) => {
                     <p>Price: ${price}</p>
                     <p>Duration: {duration} Days</p>
                     <div className="card-actions justify-center">
-                        <button onClick={()=>handleAddToCart(singleClass)} className="btn btn-outline btn-secondary border border-l-4 border-r-4">Enroll Now</button>
+                        <button onClick={() => handleAddToCart(singleClass)} className="btn btn-outline btn-secondary border border-l-4 border-r-4">Enroll Now</button>
                     </div>
                 </div>
             </div>
